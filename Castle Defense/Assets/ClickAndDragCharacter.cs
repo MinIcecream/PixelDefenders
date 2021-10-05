e15cfba501ab114a62d81bc2040520382e241399
MinIcecream <63301542+MinIcecream@users.noreply.github.com>
2021-10-04 20:30:23 -0400

    trying to fix shit

--- Castle Defense/Assets/ClickAndDragCharacter.cs
+++ Castle Defense/Assets/ClickAndDragCharacter.cs
@@ -4,16 +4,56 @@ using UnityEngine;
 
 public class ClickAndDragCharacter : MonoBehaviour
 {
-    int bitmask = 1<<13;
+    int bitmask = 1 << 13;
+
     bool dragging = false;
+
     GameObject hitObject;
+
     bool objChosen = false;
-    
+ 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 100, bitmask);
+        //Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+        if (hit.collider != null)
+        {
+            if (objChosen == false)
+            {
+                hitObject = hit.collider.gameObject;
+            }
+
+            objChosen = true;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragging = true;
+            }
+        }
+        else
+        {
+            if (dragging == false)
+            {
+                objChosen = false;
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+            objChosen = false;
+            hitObject = null;
+        }
+
+
+        if (dragging && hitObject != null)
+        {
+            if (hitObject.GetComponent<MoveWithDrag>() != null)
+            {
+                hitObject.GetComponent<MoveWithDrag>().drag = true;
+            }
         }
     }
 }
