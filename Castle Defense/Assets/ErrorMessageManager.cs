14d285f0ae0b67e977f5fe289e47824ae6fb28cd
Daniel Zhang <z331317@gmail.com>
2021-11-26 10:30:14 -0500

    movin to pc

--- Castle Defense/Assets/ErrorMessageManager.cs
+++ Castle Defense/Assets/ErrorMessageManager.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class ErrorMessageManager : MonoBehaviour
 {
-
     public static void MakeErrorMessage(string message, Vector2 pos)
     {
         GameObject newMessage = Instantiate(Resources.Load("Spawnables/ErrorMessage"), pos, Quaternion.identity) as GameObject;
         newMessage.GetComponent<ErrorMessage>().SetText(message);
+        newMessage.transform.SetParent(GameObject.FindWithTag("Canvas").transform);
     }
 }
